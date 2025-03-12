1bd8847a4338edffb3ceb2f7bbae1b5f131038e3
pjay <ipriyanshujain@gmail.com>
2025-03-13 02:32:41 +0530

    logo and copy edits (#27)

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import rehypeSlug from "rehype-slug";
-import astroI18next from "astro-i18next";
 import alpinejs from "@astrojs/alpinejs";
 import AstroPWA from "@vite-pwa/astro";
 import icon from "astro-icon";
@@ -20,7 +19,6 @@ export default defineConfig({
 	integrations: [
 		tailwind(),
 		sitemap(),
-		astroI18next(),
 		alpinejs(),
 		AstroPWA({
 			mode: "production",
