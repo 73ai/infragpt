48cca781d0468e6f9a4b224055d45dbee05dd29a
PJ <ipriyanshujain@gmail.com>
2025-03-13 02:32:13 +0530

    logo and copy edits

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
 			fontFamily: {
 				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
 			},
+			colors: {
+				primary: "#0023C4",
+			}
 		},
 	},
 	plugins: [require("@tailwindcss/typography")],
